2dc5e1615362463d3b5c7a8ca588d2a943cd3b67
Kenenn Lara <kenennlara@gmail.com>
2026-04-30 18:55:21 -0600

    Complete step 4 and 5 from project "Calculator"
    Create basic button layout and show their value when clicked.

--- calculator.js
+++ calculator.js
@@ -16,4 +16,23 @@ function operate(x, y, operator) {
   }
 }
 
+//Create the functions that update one of your
+// number variables when the calculator’s
+// digit buttons are clicked.
+// Your calculator’s display
+// should also update to reflect the value
+// of that number variable.
+
+//1. Get the value on the button and update the display
+const buttonNumbers = document.querySelector(".buttons");
+
+buttonNumbers.addEventListener("click", (e) => {
+  let target = e.target;
+  if (target.matches("button")) {
+    let value = target.innerHTML;
+
+    document.querySelector("#output").value += value;
+  }
+});
+
 operate(x, y, operator);
