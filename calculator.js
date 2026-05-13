3c6a50dc59f45e14c604cdf5862041da377bf272
Kenenn Lara <kenennlara@gmail.com>
2026-05-13 11:04:50 -0600

    Add function for getting operator
    Create a function for getting operators.

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 let x = []; //Array for getting value
-let currentNumber = 0; //First number for operation
-let operator = "-"; //Operator Input
-let y = 0; //Second number for operation
+let currentNumber = " "; //First number for operation
+let operator = " "; //Operator Input
+let secondNumber = " "; //Second number for operation
 
 //1. Make selection
 //We get a nodeList of our buttons that have class = ".btn"
@@ -10,11 +10,14 @@ const allButtons = document.querySelectorAll(".btn");
 
 const resultOutput = document.querySelector("#output");
 
+const operatorButtons = document.querySelectorAll(".btnOperator");
+
 //2. Register a click event to all of the buttons and get value
 
 function getInputValue(element, array) {
   element.forEach((bt) => {
     bt.addEventListener("click", (e) => {
+      console.log(bt);
       let value = e.target.innerHTML;
       resultOutput.value += value;
       array.push(value);
@@ -22,14 +25,35 @@ function getInputValue(element, array) {
       // Convert array -> string -> number
       currentNumber = Number(array.join("")); //Turn this into an int and updates global value
 
-      console.log(currentNumber); // This is now a single number
+      console.log(currentNumber);
+
+      //Need to clear number after selecting an operator for getting second value
+
+      // This is now a single number
     });
   });
 }
 
-getInputValue(allButtons, x);
+//2. Get operator and reset currentNumber
+function getOperator() {
+  operatorButtons.forEach((bt) => {
+    bt.addEventListener("click", (e) => {
+      let value = e.target.innerHTML;
+      let operator = e.target.innerHTML;
+      console.log(operator);
+      resultOutput.value += operator;
+    });
+  });
+}
 
 /*
+This is called asynchronous behavior:
+
+Your main script runs top → bottom
+Event listeners run later, when triggered
+*/
+
+//This function must pop when = is selected
 function operate(x, y, operator) {
   switch (operator) {
     case "+":
@@ -44,9 +68,10 @@ function operate(x, y, operator) {
   }
 }
 
-operate(x, y, operator);
+let firsInputValue = getInputValue(allButtons, x);
+let operatorInputValue = getOperator();
 
-*/
+operate(firsInputValue, secondNumber, operatorInputValue);
 
 // Make the calculator work! You’ll need to store the
 // first and second numbers input by the user and then operate() on them
